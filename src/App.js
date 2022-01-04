e920785cf7c15ef5ce69c225aea053561b3f66d7
aditi4720 <singh.aditi9698@gmail.com>
2022-01-04 15:13:52 +0530

    Added logo, text and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //BEM convention
+    <div className="app">
+      <div className="app_top">
+        {/*TOP instagram logo*/}
+        <img
+          className="app_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
+          alt="logo"
+        />
+        {/*Reels text*/}
+        <h1>Reels</h1>
+      </div>
+      <div className="app_videos">{/*container for scrollable videos */}</div>
     </div>
   );
 }
---
+++ src/VideoCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./VideoCard.css";
+
+function VideoCard() {
+  return <div></div>;
+}
+
+export default VideoCard;
